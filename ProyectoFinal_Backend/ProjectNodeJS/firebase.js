fc90178931c3a95ae79a1751a91bbf371861f3ea
JuanMontoya888 <jmaurixiomartinex@gmail.com>
2025-06-12 12:29:49 -0600

    separacion backend - frontend

--- ProyectoFinal_Backend/ProjectNodeJS/firebase.js
+++
@@ -1,7 +0,0 @@
-//Crear el enlace a la base de datos
-var admin = require("firebase-admin");
-var serviceAccount = require("./firestore-key.json");
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
-
-
-module.exports = admin;
