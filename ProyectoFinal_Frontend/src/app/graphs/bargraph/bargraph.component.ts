0dfc90a835b26218756e5581cf888bfe8391ce63
ricardorzt <ricky8464@gmail.com>
2025-06-18 15:02:16 -0600

    Components

---
+++ ProyectoFinal_Frontend/src/app/graphs/bargraph/bargraph.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bargraph',
+  imports: [],
+  templateUrl: './bargraph.component.html',
+  styleUrl: './bargraph.component.css'
+})
+export class BargraphComponent {
+
+}
---
+++ ProyectoFinal_Frontend/src/app/graphs/mainview/mainview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainview',
+  imports: [],
+  templateUrl: './mainview.component.html',
+  styleUrl: './mainview.component.css'
+})
+export class MainviewComponent {
+
+}
