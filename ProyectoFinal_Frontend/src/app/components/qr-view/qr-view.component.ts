11d7608fb6d635926a67948de80e5c209c893041
ricardorzt <ricky8464@gmail.com>
2025-06-19 10:51:22 -0600

    QR View added

--- ProyectoFinal_Frontend/src/app/components/qr-view/qr-view.component.ts
+++ ProyectoFinal_Frontend/src/app/components/qr-view/qr-view.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import {QRCodeComponent} from 'angularx-qrcode'
+import { Reservacion } from '../../models/reservacion';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-qr-view',
-  imports: [QRCodeComponent],
+  imports: [QRCodeComponent,
+    MatCardModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './qr-view.component.html',
   styleUrl: './qr-view.component.css'
 })
 export class QrViewComponent {
 
+  //variables
+  reservacion!:Reservacion;
   qrLink:string='';
 
 }
