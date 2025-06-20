3354ccae034be3c3100e0bb86790a307dd01a2be
YahirTostado <yahireduardotn6@gmail.com>
2025-06-20 11:40:02 -0600

    Mejoras Accesibilidad

--- ProyectoFinal_Frontend/src/app/components/accesibilidad/accesibilidad.component.ts
+++ ProyectoFinal_Frontend/src/app/components/accesibilidad/accesibilidad.component.ts
@@ -43,6 +43,11 @@ export class AccesibilidadComponent {
     window.speechSynthesis.speak(voz);
     this.lecturaActiva = true;
   }
+  
+  cerrarMenu() {
+    this.menuAbierto = false;
+  }
+
 
   pararLectura() {
     window.speechSynthesis.cancel();
